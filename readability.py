e5cbcfad3fbc133fe4ef772d01c60baf522d9f3e
CrispyNipsy <magnus.borregaard@gmail.com>
2019-11-04 15:23:14 +0100

    .

--- readability.py
+++ readability.py
@@ -3,18 +3,13 @@ import re
 
 # Speeches (.txt) should be named by the year in which they were spoken
 # Returns a string containing the whole transcription of the speech
-
-
 def readSpeech(year):
   # Opens file
-  text = open(f"./speeches/{year}.txt", "r", encoding="utf-8")
-  speech = text.read()
-  text.close()
-  return speech
+  with open(f"./speeches/{year}.txt", "r", encoding="utf-8") as text:
+    speech = text.read()
+    return speech
 
 # Takes a string and returns the LIX of that string
-
-
 def calculateLix(speech):
   cleanSpeech = re.sub("\n", " ", speech)
   cleanSpeech = re.sub(r"—|-", " ", cleanSpeech)
@@ -26,5 +21,5 @@ def calculateLix(speech):
 #    numLongWords =
 
 
-# ? TESTING
+#! TESTING
 calculateLix(readSpeech(2002))
