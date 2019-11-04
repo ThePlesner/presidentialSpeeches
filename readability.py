c79fe3eb0d309fad9598821edc4c9307c1d77546
CrispyNipsy <magnus.borregaard@gmail.com>
2019-11-04 15:15:24 +0100

    comments

--- readability.py
+++ readability.py
@@ -1,24 +1,30 @@
+# Importing regular expression-library
 import re
+
+# Speeches (.txt) should be named by the year in which they were spoken
+# Returns a string containing the whole transcription of the speech
 
 
 def readSpeech(year):
-    # Opens file 
-    text = open(f"./speeches/{year}.txt", "r", encoding="utf-8")
-    speech = text.read()
-    text.close()
-    return speech
-    
+  # Opens file
+  text = open(f"./speeches/{year}.txt", "r", encoding="utf-8")
+  speech = text.read()
+  text.close()
+  return speech
+
+# Takes a string and returns the LIX of that string
+
 
 def calculateLix(speech):
-    cleanSpeech = re.sub("\n", " ", speech)
-    cleanSpeech = re.sub(r"—|-", " ", cleanSpeech)
-    cleanSpeech = re.sub(r"[^a-zA-Z '-]", "", cleanSpeech)
-    
-    
-    
-    print(cleanSpeech)
-#    numWords = 
+  cleanSpeech = re.sub("\n", " ", speech)
+  cleanSpeech = re.sub(r"—|-", " ", cleanSpeech)
+  cleanSpeech = re.sub(r"[^a-zA-Z '-]", "", cleanSpeech)
+
+  print(cleanSpeech)
+#    numWords =
 #    numPeriods =
 #    numLongWords =
 
+
+# ? TESTING
 calculateLix(readSpeech(2002))
