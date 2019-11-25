651af04d5f81c1f9a6426a544d20d66938f73ec0
kare3622 <karen.wium97@gmail.com>
2019-11-25 15:12:33 +0100

    .

---
+++ gui.py
@@ -0,0 +1,34 @@
+from tkinter import *
+import clouds
+
+
+def generateWordCloud(year):
+    clouds.showWordCloud(year)
+
+    
+
+root = Tk()
+root.geometry("500x500")
+
+inputFrame = Frame(root)
+
+submitButton = Button(inputFrame, text = "submit", command = generateWordCloud(inputField.get()))
+submitButton.pack(fill = X, side = RIGHT)
+
+inputField = Entry(inputFrame)
+inputField.pack()
+
+inputFrame.pack(anchor = CENTER)
+root.mainloop()
+
+
+
+#root = Tk()
+#w = Label(root, text="Hello")
+#w.pack()
+#
+#root.mainloop()
+
+
+
+
