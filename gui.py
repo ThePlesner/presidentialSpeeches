56d3b70b1f98e232bb435c01be346393dde24c7a
Mikkel Ottosen <mikkel.mokasin@gmail.com>
2019-12-01 15:54:04 +0100

    changes to gui

--- gui.py
+++ gui.py
@@ -1,38 +1,57 @@
 from tkinter import *
-from readability import *
+import webbrowser
+import readability
+import htmlGenerator
 import clouds
+import os
 
-
-
-def generateWordCloud(year):
+def generateOne(year, root):
     clouds.showWordCloud(year)
+    window = Toplevel(root)
+    
+    speech = readability.readSpeech(year)
 
-def showLix(year):
-    speech = readSpeech(year)
+    lixNumber = readability.calculateLix(speech)
+    colemanLiauNumber = readability.calculateColemanLiau(speech)
+
+    listBox = Listbox(window)
+    listBox.pack()
+
+    listBox.insert(END, "Indexes for: " + str(year))
+    listBox.insert(END, "Lix: " + str(lixNumber))
+    listBox.insert(END, "Coleman-Liau: " + str(colemanLiauNumber))
+
+
+def generateAll():
+    htmlGenerator.writeHTML()
+    webbrowser.open_new_tab(os.getcwd() + "/output/documents/overview.html")
+
 
 root = Tk()
-root.geometry("500x500")
 
 inputFrame = Frame(root)
 
 inputField = Entry(inputFrame)
-inputField.pack(side=LEFT)
+inputField.pack(padx=(40, 40), pady=(5, 5))
 
-submitButton = Button(inputFrame, text = "wordcloud", command = lambda : generateWordCloud(inputField.get()))
-submitButton.pack(side=LEFT)
+submitButton = Button(inputFrame, text = "Generate single", command = lambda : generateOne(inputField.get(), root))
+submitButton.pack(fill = BOTH, padx=(40, 40), pady=(5, 5))
 
-lixButton = Button(inputFrame, text = "lix", )
+generateAllButton = Button(inputFrame, text= "Generate all", command = lambda : generateAll())
+generateAllButton.pack(fill = BOTH, padx=(40, 40), pady=(5, 5))
+
+infoText = Label(inputFrame, text="This is our (minimalistic) GUI. In the input field you can write a year \n" 
+                                  "between 1961 and 2019 and press 'generate single'. This will generate one \n "
+                                  "wordcloud for the State of the Union speech for that year, and create a \n"
+                                  "new window with the readability indexes. If you press 'generate all' you \n"
+                                  "will not need to write a year, as it will generate a html file and open \n"
+                                  "it in your default browser. This might take some time but you can follow\n"
+                                  "the progress in the console")
+infoText.pack()
 
 inputFrame.pack(anchor = CENTER)
 root.mainloop()
 
-
-
-#root = Tk()
-#w = Label(root, text="Hello")
-#w.pack()
-#
-#root.mainloop()
 
 
 
